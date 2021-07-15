f479fc16e7412875f61b98dbf9896b04a8a73770
Natalia Rossini <naty.rossini@hotmail.com>
2021-07-15 17:38:59 +0100

    basic react and logic implemented

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = props => {
         <Fragment>
             <header className={classes.header}>
                 <h1>The Bakery</h1>
-                <HeaderCartButton />
+                <HeaderCartButton onClick={props.onShowCart} />
             </header>
             <div className={classes['main-image']}>
                 <img src={bread} alt='sourdough bread' />
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,18 +1,40 @@
+import { useRef, useState } from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css'
 
 const MealItemForm = props => {
+    const [amountIsValid, setAmountIsValid] = useState(true);
+
+    const amountInputRef = useRef();
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const enteredAmount = amountInputRef.current.value;
+        const enteredAmountNum = +enteredAmount;
+
+        if (enteredAmount.trim().length === 0 || enteredAmountNum < 1 || enteredAmountNum > 10){
+            setAmountIsValid(false);
+            return;
+        }
+
+        props.onAddToCart(enteredAmountNum);
+    };
     return (
-        <form className={classes.form}>
-            <Input label="Quantity" input={{
-                id: 'amount' + props.id,
-                type: 'number',
-                min: '1',
-                max: '10',
-                step: '1',
-                defaultValue: '1',
+        <form className={classes.form} onSubmit={submitHandler}>
+            <Input 
+                ref={amountInputRef}
+                label="Quantity" 
+                input={{
+                    id: 'amount' + props.id,
+                    type: 'number',
+                    min: '1',
+                    max: '10',
+                    step: '1',
+                    defaultValue: '1'
             }} />
             <button>+ Add</button>
+            {!amountIsValid && <p>Please enter a valid amount (1-10)</p>}
         </form>
     )
 
