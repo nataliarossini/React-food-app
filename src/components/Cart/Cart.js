c57d9e08d8aa370ad80f615a7422a6f68e68f2ca
Natalia Rossini <47406531+nataliarossini@users.noreply.github.com>
2021-07-18 10:39:07 +0100

    Revert "Revert "Submit""

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -24,6 +24,16 @@ const Cart = (props) => {
         setIsCheckout(true);
     }
 
+    const submitOrderHandler = (userData) => {
+        fetch("https://meals-react-71efd-default-rtdb.europe-west1.firebasedatabase.app/meals.json", {
+            method: 'POST',
+            body: JSON.stringify({
+                user: userData,
+                orderedItems: cartCtx.items
+            })
+        })
+    };
+
     const cartItems =( 
         <ul className={classes['cart-items']}>
             {cartCtx.items.map(item => (
@@ -53,7 +63,7 @@ const Cart = (props) => {
                 <span>Total Amount</span>
                 <span>{totalAmount}</span>
             </div>
-            {isCheckout && <Checkout onCancel={props.onClose}/>}
+            {isCheckout && (<Checkout onConfirm={submitOrderHandler} onCancel={props.onClose}/>)}
             {!isCheckout && modalActions}
         </Modal>
     )
